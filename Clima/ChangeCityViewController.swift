c6925bfb4cb909b63a9c2bd6e559a000a2b7c562
Leo Vergnetti <tuj52136@temple.edu>
2019-10-04 11:04:55 -0400

    Initial Commit

---
+++ Clima/ChangeCityViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ChangeCityViewController.swift
+//  WeatherApp
+//
+//  Created by Angela Yu on 23/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+
+//Write the protocol declaration here:
+protocol ChangeCityDelegate {
+    func userEnteredANewCityName(city: String)
+}
+
+
+class ChangeCityViewController: UIViewController {
+    
+    //Declare the delegate variable here:
+    var delegate : ChangeCityDelegate?
+    
+    @IBOutlet weak var changeCityTextField: UITextField!
+
+    
+
+    @IBAction func getWeatherPressed(_ sender: AnyObject) {
+        
+        let cityName = changeCityTextField.text!
+        
+        delegate?.userEnteredANewCityName(city: cityName)
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+    //This is the IBAction that gets called when the user taps the back button. It dismisses the ChangeCityViewController.
+    @IBAction func backButtonPressed(_ sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
